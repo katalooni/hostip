3ee8505fccfc8301b40eaf01c5ad69e3f47b52ee
elmahdi ouhssain <elmahdi.ouhssain@gmail.com>
2018-01-30 15:28:53 +0000

    changes

--- contact/contact.php
+++ contact/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'support@urowntv.org'; 
-$sendTo = 'support@urowntv.org';
+$from = 'contact@urhd.tv'; 
+$sendTo = 'contact@urhd.tv';
 $subject = 'New message from clients';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
